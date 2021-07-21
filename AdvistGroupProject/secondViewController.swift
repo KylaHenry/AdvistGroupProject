51334137845ca9bd208fe017dade9f5dad1b2bbc
Alise <alise27bakk@icloud.com>
2021-07-21 14:53:50 -0400

    I had to move the constraint  for view our stories to click the button when I ran it

---
+++ AdvistGroupProject/secondViewController.swift
@@ -0,0 +1,31 @@
+//
+//  secondViewController.swift
+//  AdvistGroupProject
+//
+//  Created by  Scholar on 7/21/21.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func firstURL(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
