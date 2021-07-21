4d2f0a223c4044057768598ce85334f1b536041f
Group Project <kylachenry15@gmail.com>
2021-07-21 12:15:29 -0400

    I completed the first homepage along with the loading page by adding the logo and the indicator. I also need help fitting the paragraph onto the first homepage.

--- AdvistGroupProject/ViewController.swift
+++ AdvistGroupProject/ViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func viewOurStoriesButton(_ sender: Any) {
+    }
+    
+    @IBAction func sourcesButton(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
