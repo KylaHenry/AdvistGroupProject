155782433c16327d504dd2d8d7f3d0d760ed432e
Group Project <kylachenry15@gmail.com>
2021-07-22 16:15:12 -0400

    I fixed the constraints on the main homepage and the second view controller. So everything there is perfectly fine. I just need to link them to the toolbar.

--- AdvistGroupProject/ViewController.swift
+++ AdvistGroupProject/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
     
    
+    @IBAction func toolBarButton(_ sender: Any) {
+    }
     
     @IBAction func sourcesButton(_ sender: Any) {
     }
--- AdvistGroupProject/secondViewController.swift
+++ AdvistGroupProject/secondViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class secondViewController: UIViewController {
     
     
+    
+    
     @IBAction func test(_sender: Any){
         print("test")
     }
